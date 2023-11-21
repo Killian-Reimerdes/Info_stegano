686cdb6bf65252fcfa537ca54095cc1bdf0bbe1b
Killian-Reimerdes <Killian.Reimerdes@gmail.com>
2023-11-21 09:35:43 +0100

    get pixel 2x2
    get pixel 2x2

--- main_code.py
+++ main_code.py
@@ -1,5 +1,14 @@
 print("Hello world")
 #je suis une catin
 
+from PIL import Image
+
+im = Image.open("Red_square.png")
+pix = im.load()
+a,b = im.size
+pixels = [[pix[i,j] for j in range(a)]for i in range(a)]
+print(pixels[0])
+print(pixels[1])
+
 
 
