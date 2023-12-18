90d0c20273dbcfcc44bc6fb6102f6c0bb8b3e0bd
ArtPitGym <po99xzh@eduvaud.ch>
2023-12-19 00:04:08 +0100

    Question comment fonction l'interface python en dehors des notebooks

--- ArthurTryNotNotebook.py
+++ ArthurTryNotNotebook.py
@@ -1,5 +1,16 @@
-print("this is a file.py without cells")
+#print("this is a file.py without cells")
+#print("salut Kylian")
+from PIL import Image
 
-print("salut Kylian")
+imart = Image.new("RGB", (100,100),(100, 200, 15)) #tout doit être des tuples
+#imart.show()
+
+size = imart.size
+color = imart.mode
+
+print(imart.mode)
+print(imart.format) #format je devrais mettre png
+
+imart.save("ImartYes.png")
 
 
