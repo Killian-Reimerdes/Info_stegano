6e88ab659eea70c33bb47ee889a0498292788752
ArtPitGym <po99xzh@eduvaud.ch>
2023-12-25 11:55:10 +0100

    Une fonction qui prend comme argument une lettre et qui return son ascii en binaire
    Plustard, je vais faire en sorte que ça fonctionne pour les chaînes de caractères aussi. Dcp des phrases.

--- ArthurTryNotNotebook.py
+++ ArthurTryNotNotebook.py
@@ -1,18 +1,40 @@
 #print("this is a file.py without cells")
-#print("salut Kylian")
-from PIL import Image
+#print("salut Killian")
+#from PIL import Image
 
-imart = Image.new("RGB", (100,100),(100, 200, 15)) #tout doit être des tuples
-imart.show()
+#imart = Image.new("RGB", (100,100),(100, 200, 15)) #tout doit être des tuples
+#imart.show()
 
-size = imart.size
-color = imart.mode
+#size = imart.size
+#color = imart.mode
 
-print(imart.mode)
-print(imart.format) #format je devrais mettre png
+#print(imart.mode)
+#print(imart.format) ##format je devrais mettre png
 
-imart.save("ImartYes.png")
+#imart.save("ImartYes.png")
 
-#changement 
+##changement 
 
+def test_trans_msg_ascII_bin():
+    assert trans_msg_ascII_bin("a") == "0b1100001"
+    assert trans_msg_ascII_bin("R") == "0b1010010"
+    assert trans_msg_ascII_bin("aR") == "0b"
+    print("Everything works!")
 
+def trans_msg_ascII_bin(Msg):
+    """
+    Cette fonction prend comme argument une lettre et return son numéro acsII en binaire
+    """
+    num_Msg = ord(Msg)
+    bin_Msg = bin(num_Msg)
+    
+    return bin_Msg
+
+test_trans_msg_ascII_bin()
+
+#je vais faire la suite pour une chaîne de caractère
+
+#num = ord("a")
+#bin_a = bin(num)
+#print(bin_a)
+#print(type(bin_a))
